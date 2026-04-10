18b5dd023e64b91423c3da5986e1bc05f9dff3d9
SamuelProgrammingBest <adewalesamuel835@gmail.com>
2026-04-10 16:45:07 +0100

    Added cors

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ connectMyDb();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: 'http://localhost:5173', // Allow your React app
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
 );
